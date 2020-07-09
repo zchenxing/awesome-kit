e7231d0572d9edf89bfc8bdc4ab4ae59287308ec
zyj7815 <yuanjie.zhu@ktbhealth.com>
2020-07-09 22:38:12 +0800

    feat(): 重写打包

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',    // 入口文件
+    output: {
+        filename: "index.js",
+        path: path.join(__dirname, 'lib'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                include: [
+                    path.join(__dirname, 'src')
+                ],
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+}
